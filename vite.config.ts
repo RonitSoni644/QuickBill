9cdc73a36b36db277320d5c3f2117332fe854f19
RonitSoni644 <ronitsoni505@gmail.com>
2026-04-07 14:20:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: process.env.VITE_BASE_PATH || "/Invoicehub",
+  base: process.env.VITE_BASE_PATH || "/Invoicepro",
   resolve: {
     alias: {
       // Alias @ to the src directory
